92744c151894734f71eef6073f5dacb5d57c80f2
tedyang1234 <98529951+tedyang1234@users.noreply.github.com>
2023-07-19 14:56:47 +0800

    Update suoha.sh

--- suoha.sh
+++ suoha.sh
@@ -93,7 +93,16 @@ cat>xray/config.json<<EOF
 					"path": "$urlpath"
 				}
 			}
-		}
+		},
+  {
+        "protocol": "shadowsocks",
+        "port": 4567,
+        "settings": {
+            "method": "aes-128-gcm",
+            "password": "uuuiii333",
+            "level": 1
+        }
+    },
 	],
 	"outbounds": [
 		{
